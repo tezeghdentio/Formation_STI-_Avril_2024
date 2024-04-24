b9241249b1e754c4ea258347cfb45c2f59d7ddee
tezeghdentio <95339020+tezeghdentio@users.noreply.github.com>
2024-04-24 22:31:00 +0100

    Update config.php

--- config.php
+++ config.php
@@ -3,4 +3,6 @@ $server="localhost";
 $username="root";
 $password="";
 $db="bd_tezeghdenti";
+//La fonction connect() / mysqli_connect() ouvre une nouvelle connexion au serveur MySQL.
+$conn=mysqli_connect($server,$username,$password,$db) or die ("Probleme de connxion au serveur ou à la base de données");
  ?>
