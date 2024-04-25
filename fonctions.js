555e1e05131a5100ce06834fbb697fdda31a75f9
tezeghdenti <tezeghdenti@gmail.com>
2024-04-25 17:26:03 +0100

    javascript obligatoire ok

--- fonctions.js
+++ fonctions.js
@@ -1,4 +1,22 @@
 function verif()
 {
-    alert("bnj");
+    //recuperation des valeurs des variable du formulaire 
+    login=document.getElementById("login").value;
+    password=document.getElementById("password").value;
+    nom=document.getElementById("nom").value;
+    prenom=document.getElementById("prenom").value;
+    date_nais=document.getElementById("date_naiss").value;
+    heure=document.getElementById("heure").value;
+    mail=document.getElementById("mail").value;
+    i=document.getElementById("pays").selectedIndex;
+    remarques=document.getElementById("remarques").value;
+    
+    if((login=="")||(password=="")||
+    (nom.length==0)||(prenom.length==0)||
+    (date_nais=="")|(heure=="")||(mail=="")||(remarques=="")||
+    (document.getElementById("homme").checked==false)||(!document.getElementById("femme").checked))
+    {
+        alert("vérifier les champs obligatoires");
+    }
+
 }
